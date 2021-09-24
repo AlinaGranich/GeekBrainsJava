4f809424d94d3103188b6b4a5d1d1485e976d74b
AlinaGranich <68391858+AlinaGranich@users.noreply.github.com>
2021-09-24 22:37:45 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Employee employee = new Employee(
-                "Колтунов.A",
+                "Саша.A",
                 "Programmer",
                 "Kol@mail.ru",
                 "89214567890",
